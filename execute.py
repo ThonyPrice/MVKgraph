15d02dcb4afeec1b2ee2abd575d45e1a7cb98222
Thony Price <thonyprice@gmail.com>
2017-02-02 13:31:55 +0100

    Added file execute.py to more easily try out written functions

---
+++ execute.py
@@ -0,0 +1,17 @@
+# This file can be used to try execute the existing parts of the software
+from download import *
+
+"""
+Print the dictionary, course codes are keys and dependencies for the 
+course are values. Comment in the print statement in getEligibility in
+download.py to alse show the eligibility text from kth.se
+"""
+# eligebility_dict = getEligibilityDict()
+# for i in eligebility_dict:
+#     print(i)
+
+"""
+Insert course code to show it's eligibility
+"""
+# getEligibility('DD1396')
+
