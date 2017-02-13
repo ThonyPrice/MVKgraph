11d4fcb9f5aae37bba7c88fb9b50ca0b39b2b274
Thony Price <thonyprice@gmail.com>
2017-02-13 14:22:01 +0100

    Removed uneccesary for loop

--- execute.py
+++ execute.py
@@ -1,17 +1,15 @@
 # This file can be used to try execute the existing parts of the software
-from download import *
+from download import getEligibility
+from download import getEligibilityDict
 
 """
 Print the dictionary, course codes are keys and dependencies for the 
 course are values. Comment in the print statement in getEligibility in
 download.py to alse show the eligibility text from kth.se
 """
-# eligebility_dict = getEligibilityDict()
-# for i in eligebility_dict:
-#     print(i)
+eligebility_dict = getEligibilityDict()
 
 """
 Insert course code to show it's eligibility
 """
 # getEligibility('DD1396')
-
