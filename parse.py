db1d45f07ef653aa0508b409c69315907fe16828
Thony Price <thonyprice@gmail.com>
2017-02-14 17:23:58 +0100

    + Rarsing for recommended courses and code clean up

--- parse.py
+++ parse.py
@@ -9,31 +9,51 @@ Parse text for dependent courses and credits
 @return DependencyObject containing dependent courses and credits
 """
 
-def parseText (text):
+def parseText(text):
     Dependency = DependencyObject()
+    Dependency.courses = getDepCourses(text)
+    depCredits = getDepCredits(text)
+    Dependency.setCredits(depCredits)
+    recCourses = getRecomendCourses(text)
+    Dependency.getRecommended(recCourses)
+    print(Dependency)
+    return Dependency
+    
+def getDepCourses(text):    
     course = Regex('([A-Z]{2}|\d[A-Z])(\d{4}|\d{3}[A-Z])')
-    or_words = oneOf('or /', caseless=True)
-    and_words = oneOf('and +', caseless=True)
+    orWord = oneOf('or eller /', caseless=True)
     signs = oneOf('- , . ')
-    keywords = OneOrMore(   \
-                    Suppress(Optional(or_words)) + course + \
-                    Suppress(Optional(OneOrMore(~or_words + \
+    andWord = oneOf('and + och also', caseless=True)
+    keywords =  OneOrMore(\
+                    Suppress(Optional(orWord)) + course + \
+                    Suppress(Optional(OneOrMore(~orWord + \
                         (Word(alphas)^signs)))) + \
-                    OneOrMore(Suppress(or_words) + course) + \
+                    OneOrMore(Suppress(orWord) + course) + \
                     Suppress(Optional(OneOrMore((Word(alphas) ^ \
-                        signs) + ~(and_words + course) + \
-                        ~(Optional(OneOrMore(alphas)) + and_words + \
+                        signs) + ~(andWord + course) + \
+                        ~(Optional(OneOrMore(alphas)) + andWord + \
                         (Optional(OneOrMore(alphas)))))) + \
-                    Optional(Suppress(or_words) + course))) \
+                    Optional(Suppress(orWord) + course))) \
                     ^ OneOrMore(course)
-                        
+    return keywords.searchString(text).asList()
+    
+def getDepCredits(text):    
+    eduLevel = Regex('[P|p]h[d|.\s[D|d].?') ^ CaselessLiteral('Bacherlor')        
+    # TODO: Credits can be represented with commas (e.g. 7,5 hp)    
     credits =       (~(CaselessLiteral('single course students:')) + \
                     Word(nums, max=3) + Optional('of the') + \
-                    oneOf('university credits ects hp', caseless=True) + \
+                    (oneOf('university credits ects hp', caseless=True) ^
+                    CaselessLiteral('higher education credits')) + \
                     SkipTo('.', include=False)) ^ \
-                    (CaselessLiteral('phd') + SkipTo('.'))
+                    (eduLevel + SkipTo('.'))
+    return credits.searchString(text)
+    
+def getRecomendCourses (text):
+    startWord = Regex('[A-Z][a-z]+') ^ Suppress(oneOf('. ,'))    
+    recommended =   (MatchFirst(startWord) \
+                    ^ OneOrMore(~CaselessLiteral('Recommended') + \
+                    Word(alphas))) + \
+                    CaselessLiteral('Recommended') + \
+                    SkipTo('.')
+    return recommended.searchString(text)
 
-    Dependency.courses = keywords.searchString(text).asList()
-    Dependency.setCredits(credits.searchString(text))
-    # print(dependency)
-    return Dependency
