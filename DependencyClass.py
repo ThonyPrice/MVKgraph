e0f19ff5c752b4da44e3af23a34c0082b0db5ad0
Thony Price <thonyprice@gmail.com>
2017-02-14 17:22:45 +0100

    + Attribute to store recommended courses

--- DependencyClass.py
+++ DependencyClass.py
@@ -1,20 +1,44 @@
+import re
+
+# TODO: Special characters to replace while adding data;
+# &#64257; == fi
+# &#160; = (non-breaking space) 
+# &#180; = ´ 
+# &#183; = ·
+# &#229; = å 
+# &#228; = ä  
+# &#246; = ö
+# &#8217; = ’
+# &#8226; = •
 
 class DependencyObject():
     
     def __init__ (self):
         self.courses = []
         self.credits = "No other requirements"
+        self.recommend = "No recomendations"
     
     def setCredits (self, credits):
+        # TODO: Correct special characters (e.g. &#160)
         if len(credits) > 0:
-            self.credits = " ".join(credits[0])
-
+            self.credits = re.sub('</[li]><li>', ' ', " ".join(credits[0]))
+        
+    def getRecommended (self, rec):
+        # TODO: Correct special characters (e.g. &#160)
+        if len(rec):
+            tmp = ""
+            for x in rec:
+                tmp += " ".join(x) + ". "
+            self.recommend = tmp
+            
     # Debug representation
     def __str__ (self):
         return "DepObj; \nCourses: {courses}\
-                        \nCredits: {credits}".format(
+                        \nCredits: {credits}\
+                        \nRecomen: {recommend}".format(
             courses = self.courses, 
-            credits = self.credits
+            credits = self.credits,
+            recommend = self.recommend
         )
         
     def __repr__(self):
