2df46a4f3f8c873ec66cfd533813ec9f6c80b6f2
Thony Price <thonyprice@gmail.com>
2017-02-16 15:19:34 +0100

    Courses that have been debugged from some deparents are included

--- execute.py
+++ execute.py
@@ -7,9 +7,50 @@ Print the dictionary, course codes are keys and dependencies for the
 course are values. Comment in the print statement in getEligibility in
 download.py to alse show the eligibility text from kth.se
 """
-eligebility_dict = getEligibilityDict()
+# eligebility_dict = getEligibilityDict()
 
 """
 Insert course code to show it's eligibility
+These courses have been used for debugging, do not remove them
 """
-# getEligibility('DD1396')
+
+# getEligibility('XXXXXX')
+
+# getEligibility('ID1019') - Solved!
+# getEligibility('IS3200') - Solved!
+# getEligibility('ID2212') - Solved!
+# getEligibility('IK3616') - Solved!
+# getEligibility('ID3011') # <- Space in course, bother fixing?
+# getEligibility('IS1500') #TODO: Specal case, ambiguous meaning of or/and
+# getEligibility('ID2220') - Solved!
+# getEligibility('ID2209') - Solved!
+# getEligibility('ID2201') - Solved!
+# getEligibility('ID2210') - Solved!
+
+# getEligibility('SF3960') - Solved!
+# getEligibility('ME3538') - Solved!
+# getEligibility('SF3608') - Solved!
+# getEligibility('SF1634') - Solved!
+# getEligibility('SF250X') - Solved!
+# getEligibility('SF298X') - Solved!
+# getEligibility('SF290X') - Solved!
+# getEligibility('SF2738') - Solved!
+# getEligibility('SF2943') - Solved!
+# getEligibility('ME2016') - Solved!
+
+# getEligibility('DA222X') - Solved!
+# getEligibility('DD221V') - Solved!
+# getEligibility('DD3447') - Solved!
+# getEligibility('DD245X') - Solved!
+# getEligibility('DD226X') - Solved!
+# getEligibility('DD3313') - Solved!
+# getEligibility('DD1051') - Solved!
+# getEligibility('DD2354') - Solved!
+# getEligibility('DD2450') - Solved!
+# getEligibility('DD2396') - Solved!
+# getEligibility('DD3013') - Solved!
+# getEligibility('SF3706') - Solved!
+# getEligibility('SF3961') - Solved!
+# getEligibility('SF3940') - Solved!
+# getEligibility('SF2863') <- Two recomendations
+# getEligibility('SF3862') - Solved!
