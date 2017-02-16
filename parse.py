29080951e0ea85cf43aa8bdad5420ef7b8587b0e
Thony Price <thonyprice@gmail.com>
2017-02-16 16:58:28 +0100

    Solved merge conflicts - parse.py up to date

--- parse.py
+++ parse.py
@@ -52,20 +52,11 @@ def getDepCredits(text):
     text += '.'
     points = Regex('[1-9](\.|,)?\d+')
     eduLevel = Regex('[P|p]h\.?\s?[Dd]\.?') ^ oneOf('Bachelor Doctoral', caseless=True) ^ Regex('[M|m]aster((\')?s)?') 
-=======
-def getDepCredits(text):
-    points = Regex('[1-9](\.|,)?\d+')
-    eduLevel = Regex('[P|p]h\.?\s?[Dd]\.?') ^ CaselessLiteral('Bachelor') ^ Regex('[M|m]aster((\')?s)?') ^ CaselessLiteral('Doctoral')
->>>>>>> 6290569c548955aa7267c33e283b932eb179b6e6
     amount = CaselessLiteral('At least') ^ CaselessLiteral('Completed')
     notOfInterest = CaselessLiteral('single course students:') ^ CaselessLiteral('Non-program students,')
     participation = CaselessLiteral('Admitted to') ^ CaselessLiteral('Enrolled at')
     specCase1 = CaselessLiteral('All courses that are required for issuing the Degree of')
-<<<<<<< HEAD
     specCase2 = CaselessLiteral('Accepted to')
-=======
-    # TODO: Credits can be represented with commas (e.g. 7,5 hp)
->>>>>>> 6290569c548955aa7267c33e283b932eb179b6e6
     credits =       MatchFirst(Suppress( notOfInterest + SkipTo('.'))) \
                     ^ (Optional(amount) + points + Optional('of the') + \
                     (oneOf('university credits ects hp', caseless=True) ^
@@ -84,12 +75,8 @@ Parse for recommended courses in text.
 """
 
 def getRecomendCourses (text):
-<<<<<<< HEAD
     text += '.'
     startWord = Regex('[A-Z][a-z]+') ^ Suppress(oneOf('. ,'))    
-=======
-    startWord = Regex('[A-Z][a-z]+') ^ Suppress(oneOf('. ,'))
->>>>>>> 6290569c548955aa7267c33e283b932eb179b6e6
     recommended =   (MatchFirst(startWord) \
                     ^ OneOrMore(~CaselessLiteral('Recommended') + \
                     Word(alphanums))) + \
