db66a78a5c354194b7d9ebc659dc787d7f5da24f
wska <william.skagerstrom@gmail.com>
2017-02-16 14:17:34 +0100

    Removed additional tags with rmBadEncoding

--- DependencyClass.py
+++ DependencyClass.py
@@ -2,27 +2,27 @@ import re
 
 # TODO: Special characters to replace while adding data;
 # &#64257; == fi
-# &#160; = (non-breaking space) 
-# &#180; = ´ 
+# &#160; = (non-breaking space)
+# &#180; = ´
 # &#183; = ·
-# &#229; = å 
-# &#228; = ä  
+# &#229; = å
+# &#228; = ä
 # &#246; = ö
 # &#8217; = ’
 # &#8226; = •
 
 class DependencyObject():
-    
+
     def __init__(self):
         self.courses = []
         self.credits = ""
         self.recommend = ""
-    
+
     def setCredits(self, credits):
         # TODO: Correct special characters (e.g. &#160)
         if len(credits) > 0:
             self.credits = self.rmBadEncoding(" ".join(credits[0]))
-        
+
     def setRecCourses(self, rec):
         # TODO: Correct special characters (e.g. &#160)
         if len(rec):
@@ -30,20 +30,20 @@ class DependencyObject():
             for x in rec:
                 tmp += " ".join(x) + ". "
             self.recommend = tmp
-    
+
     def rmBadEncoding(self, string):
         string = re.sub('\s&#8217;', "'", string)
-        string = re.sub('</li><li>|<p>|</p>', ' ', string)
+        string = re.sub('</li>|<li>|<p>|</p>|<strong>|<\strong>|<ul>|<\ul>', ' ', string)
         return string
-            
+
     # Debug representation
     def __str__ (self):
         return "DepObj; \nCourses: {courses}\
                         \nCredits: {credits}\
                         \nRecomen: {recommend}".format(
-            courses = self.courses, 
+            courses = self.courses,
             credits = self.credits,
             recommend = self.recommend)
-        
+
     def __repr__(self):
         return self.__str__()
