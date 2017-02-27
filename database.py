2196ca6552f5d9d6dd020fc853fc3219d3b0ee18
Thony Price <thonyprice@gmail.com>
2017-02-27 12:43:41 +0100

    Initial commit for db

---
+++ database.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+es = Elasticsearch()
+
+courses = {
+    "DD1234": {
+        "name": "programmering",
+        "hp": 0,
+        "eligibility": {
+            "courses": [[]],
+            "credits": "",
+            "recommend": ""
+        }
+    },
+    "DD1122": {
+        "name": "databas",
+        "hp": 0,
+        "eligibility": {
+            "courses": [[]],
+            "credits": "",
+            "recommend": ""
+        }
+    }
+}
+
+for courseID, course in courses.items():
+    course["courseID"] = courseID
+    cID = ""
+    for s in courseID:
+            if s.isalpha():
+                    cID += str(ord(s))
+            else:
+                    cID += s
+    res = es.index(index="courses", doc_type="course", id=int(cID), body=course)
+
+
+# 
+# doc = {
+#     'author': 'kimchy',
+#     'text': 'Elasticsearch: cool. bonsai cool.',
+#     'timestamp': datetime.now(),
+# }
+# res = es.index(index="courses", doc_type='course', id="DD1234", body=doc)
+# print(res['created'])
+# 
+# res = es.get(index="test-index", doc_type='tweet', id=1)
+# print(res['_source'])
+# 
+# es.indices.refresh(index="test-index")
+# 
+# res = es.search(index="test-index", body={"query": {"match_all": {}}})
+# print("Got %d Hits:" % res['hits']['total'])
+# for hit in res['hits']['hits']:
+#     print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
